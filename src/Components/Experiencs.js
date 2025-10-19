99fc96d6260557dd1ab5600ba980e4e18cbe8257
Chetan Nagmoti <tech@binnys.in>
2025-10-19 10:51:13 +0530

    Update  Some New Chnages

--- src/Components/Experiencs.js
+++ src/Components/Experiencs.js
@@ -1,127 +1,185 @@
-import React from 'react'
+import React from "react";
 
-const experienceDetails=[
-    {
-      id:1,
-      year:'2023 - Present',
-      position:'MERN Full Stack Developer',
-      companyInfo:{
-        companyName:'SaveEat',
-        companyDetails:`At SaveEat, we share a common goal of 🌱 creating a zero food waste India. Together, we can benefit the 🍴 customers, the 🍽️ restaurants, and the 🌍 planet. Let's join hands 👫 for a better future.`,
-        workingDetails:[
-          {
-            info:1,
-            details:`Developed a comprehensive BrandPanel system tailored for restaurant owners, facilitating efficient management of branding assets and strategies.`
-          },
-          {
-            info:2,
-            details:`Engineered an intuitive Admin Panel empowering company officials to oversee all aspects of restaurant operations seamlessly.`
-          },
-          {
-            info:3,
-            details:`Successfully crafted a website for Binnys Jewelry, a subsidiary of Save Eat, ensuring an engaging online presence and seamless user experience.`
-          },
-          {
-            info:4,
-            details:`Provided robust backend support for the Save Eat user application, collaborating closely with React Native developers to integrate seamlessly functioning APIs.`
-          }
-  
-        ],
-        projectInCompany:[
-          {
-            project:1,
-            title:'Brand Panle',
-            projectLink:`http://businessportal.saveeat.in/`
-          },
-          {
-            project:2,
-            title:'Binnys Jewellary',
-            projectLink:`https://binnysgroups.netlify.app/`
-          }
-        ],
-        companyWebSiteLink:`https://saveeat.in/`,
-        tileCompnayLink:'Company Link',
+const experienceDetails = [
+  {
+    id: 1,
+    year: "Jun 2023 – Present",
+    position: "Senior Full Stack Developer (Team Lead)",
+    companyInfo: {
+      companyName: "Binny’s Jewellery Private Limited",
+      companyDetails: `Binny’s Jewellery Private Limited specializes in crafting bespoke jewellery pieces tailored to each customer’s unique style and preferences. From elegant designs to personalized creations, we bring your jewellery dreams to life with precision and artistry.`,
+      workingDetails: [
+        {
+          info: 1,
+          details: `Developed a scalable B2B product using React.js and Node.js, building clean, reusable components while ensuring optimal
+                     performance and scalability.`,
+        },
+        {
+          info: 2,
+          details: `Implemented role-based authentication and authorization in the backend using Node.js and JWT tokens, securing APIs and
+                     enforcing security best practices to protect user data.`,
+        },
+        {
+          info: 3,
+          details: `Optimized state management using React Hooks and Context API to reduce re-renders and improve efficiency.`,
+        },
+        {
+          info: 4,
+          details: `Enhanced front-end performance with code splitting, lazy loading, and bundle size reduction to improve software
+                     reliability.`,
+        },
+      ],
+      projectInCompany: [
+        {
+          project: 1,
+          title: "Binny’s Vision Dashboard",
+          projectLink: `https://vision.binnys.app`,
+        },
+        {
+          project: 3,
+          title: "Binny’s Select Portal",
+          projectLink: `https://select.binnys.app`,
+        },
+        {
+          project: 5,
+          title: "Binny’s Create Portal",
+          projectLink: `https://create.binnys.app`,
+        },
+        {
+          project: 4,
+          title: "My Binny’s App",
+          projectLink: `https://play.google.com/store/apps/details?id=com.binnysapp.mybinnys&pcampaignid=web_share`,
+        },
+        {
+          project: 2,
+          title: "Binny’s Scan Application",
+          projectLink: `https://play.google.com/store/apps/details?id=com.binnysapp.binnysscan&pcampaignid=web_share`,
+        },
+      ],
+      companyWebSiteLink: `https://binnysjewellery.com/`,
+      tileCompnayLink: "Company Link",
     },
-    skills:`REST APIs · JavaScript · Express.js · Object-Oriented Programming (OOP) · HTML · Moongo-DB · Node.js · React.js · Postomn`
+    skills: `REST APIs · JavaScript · Express.js · Object-Oriented Programming (OOP) · HTML · Moongo-DB · Node.js · React.js · Postomn`,
+  },
+  {
+    id: 2,
+    year: "Jun 2022 – May 2023",
+    position: "Junior MERN Full Stack Developer",
+    companyInfo: {
+      companyName: "Save-Eat Foods Private Limited",
+      companyDetails: `At SaveEat, we share a common goal of 🌱 creating a zero food waste India. Together, we can benefit the 🍴 customers, the 🍽️ restaurants, and the 🌍 planet. Let's join hands 👫 for a better future.`,
+      workingDetails: [
+        {
+          info: 1,
+          details: `Developed a comprehensive BrandPanel system tailored for restaurant owners, facilitating efficient management of branding assets and strategies.`,
+        },
+        {
+          info: 2,
+          details: `Engineered an intuitive Admin Panel empowering company officials to oversee all aspects of restaurant operations seamlessly.`,
+        },
+        {
+          info: 3,
+          details: `Successfully crafted a website for Binnys Jewelry, a subsidiary of Save Eat, ensuring an engaging online presence and seamless user experience.`,
+        },
+        {
+          info: 4,
+          details: `Provided robust backend support for the Save Eat user application, collaborating closely with React Native developers to integrate seamlessly functioning APIs.`,
+        },
+      ],
+      projectInCompany: [
+        // {
+        //   project: 1,
+        //   title: "Brand Panle",
+        //   projectLink: `http://businessportal.saveeat.in/`,
+        // },
+        // {
+        //   project: 2,
+        //   title: "Binnys Jewellary",
+        //   projectLink: `https://binnysjewellery.com/`,
+        // },
+      ],
+      companyWebSiteLink: `https://saveeat.in/`,
+      tileCompnayLink: "Company Link",
     },
-    {
-      id:2,
-      year:'2020-2023',
-      position:'Full Stack Developer',
-      companyInfo:{
-        companyName:'Freelance | Self Employed',
-        companyDetails:`Worked as a freelance software developer specializing in Core Java, Advanced Java, MySQL Database, 
+    skills: `REST APIs · JavaScript · Express.js · Object-Oriented Programming (OOP) · HTML · Moongo-DB · Node.js · React.js · Postomn`,
+  },
+  {
+    id: 3,
+    year: "Jun 2020 – April 2023",
+    position: "Full Stack Developer",
+    companyInfo: {
+      companyName: "Freelance | Self Employed",
+      companyDetails: `Worked as a freelance software developer specializing in Core Java, Advanced Java, MySQL Database, 
         JavaScript, RESTful API, Postman, React.js, TypeScript, .NET Core, ASP.NET Core, ADO.NET, and Node.js.`,
-        workingDetails:[
-          {
-            info:1,
-            details:`Resolved technical issues promptly to ensure high-quality project delivery.`
-          },
-          {
-            info:2,
-            details:`Developed scalable applications using Core Java, Advanced Java, and MySQL Database.`
-          },
-          {
-            info:3,
-            details:`Created interactive user interfaces using JavaScript and React.js.`
-          },
-          {
-            info:4,
-            details:`Designed and implemented RESTful APIs, ensuring seamless communication between systems.`
-          },
-          {
-            info:5,
-            details:`Conducted API testing and documentation using Postman.`
-          },
-          {
-            info:6,
-            details:`Collaborated with clients to gather requirements and deliver customized solutions.`
-          },
-          {
-            info:7,
-            details:`Utilized TypeScript for enhanced code quality and maintainability.`
-          },
-          {
-            info:8,
-            details:`Built server-side applications and APIs with Node.js.`
-          },
-          {
-            info:9,
-            details:`Resolved technical issues promptly to ensure high-quality project delivery.`
-          },
-         
-  
-        ],
-        projectInCompany:[
-          {
-            project:1,
-            projectLink:`Project-1 Link`
-          },
-          {
-            project:2,
-            projectLink:`Project-2 Link`
-          }
-        ],
-        companyWebSiteLink:`Company Link`,
-        tileCompnayLink:'Company Link',
+      workingDetails: [
+        {
+          info: 1,
+          details: `Resolved technical issues promptly to ensure high-quality project delivery.`,
+        },
+        {
+          info: 2,
+          details: `Developed scalable applications using Core Java, Advanced Java, and MySQL Database.`,
+        },
+        {
+          info: 3,
+          details: `Created interactive user interfaces using JavaScript and React.js.`,
+        },
+        {
+          info: 4,
+          details: `Designed and implemented RESTful APIs, ensuring seamless communication between systems.`,
+        },
+        {
+          info: 5,
+          details: `Conducted API testing and documentation using Postman.`,
+        },
+        {
+          info: 6,
+          details: `Collaborated with clients to gather requirements and deliver customized solutions.`,
+        },
+        {
+          info: 7,
+          details: `Utilized TypeScript for enhanced code quality and maintainability.`,
+        },
+        {
+          info: 8,
+          details: `Built server-side applications and APIs with Node.js.`,
+        },
+        {
+          info: 9,
+          details: `Resolved technical issues promptly to ensure high-quality project delivery.`,
+        },
+      ],
+      projectInCompany: [
+        {
+          project: 1,
+          projectLink: `Project-1 Link`,
+        },
+        {
+          project: 2,
+          projectLink: `Project-2 Link`,
+        },
+      ],
+      companyWebSiteLink: `https://chetanbnagmoti.github.io/My_Profile_Web/`,
+      tileCompnayLink: "Company Link",
     },
-    skills:`Spring Boot · ASP.NET MVC · REST APIs · C# · Java · JavaScript · Express.js · Object-Oriented Programming (OOP) · HTML · MySQL · Node.js · React.js · Maven`
-    }
-  ] 
-
+    skills: `Spring Boot · ASP.NET MVC · REST APIs · C# · Java · JavaScript · Express.js · Object-Oriented Programming (OOP) · HTML · MySQL · Node.js · React.js · Maven`,
+  },
+];
 
 const Experiencs = () => {
-
   return (
     <>
-     <div className="col-lg-12 text-center resume mb-5">
-            <h1 className="pt-5 pb-2">Experience</h1>
-            <hr className="custom-hr" />
-            <div className="container mt-5">
-              <div className="row justify-content-center">
-              {
-                experienceDetails && experienceDetails.map((experienc)=>(
-                  <div key={experienc.id+experienc.companyInfo.companyName} className="col-lg-6 col-md-12 mb-3">
+      <div className="col-lg-12 text-center resume mb-5">
+        <h1 className="pt-5 pb-2">Experience</h1>
+        <hr className="custom-hr" />
+        <div className="container mt-5">
+          <div className="row justify-content-center">
+            {experienceDetails &&
+              experienceDetails.map((experienc) => (
+                <div
+                  key={experienc.id + experienc.companyInfo.companyName}
+                  className="col-lg-6 col-md-12 mb-3"
+                >
                   <div
                     className="card text-white bg-dark shadow-lg"
                     style={{ maxWidth: "100%" }}
@@ -139,50 +197,55 @@ const Experiencs = () => {
                       </p>
                       <div className=" text-muted text-left">
                         <ul className="">
-                          {
-                            experienc.companyInfo.workingDetails.map((work,index)=>(
-                          <li key={work.info+index*2} className="">
-                                  {work.details}
-                          </li>
-
-                            ))
-                          }
+                          {experienc.companyInfo.workingDetails.map(
+                            (work, index) => (
+                              <li key={work.info + index * 2} className="">
+                                {work.details}
+                              </li>
+                            )
+                          )}
                         </ul>
                       </div>
                       <div class=" text-left d-flex flex-column">
-                        {
-                          experienc.companyInfo.projectInCompany.map((projectLink,index)=>(
-                        <a key={`${projectLink.project+projectLink.projectLink+index}`} href={`${projectLink.projectLink}`} class="text-warning" target='_blank'>
-                          {projectLink.title}
-                        </a>
-
-                          ))
-                        }
-                        <a href={`${experienc.companyInfo.companyWebSiteLink}`} class="text-info" target='_blank'>
+                        {experienc.companyInfo.projectInCompany.map(
+                          (projectLink, index) => (
+                            <a
+                              key={`${
+                                projectLink.project +
+                                projectLink.projectLink +
+                                index
+                              }`}
+                              href={`${projectLink.projectLink}`}
+                              class="text-warning"
+                              target="_blank"
+                            >
+                              {projectLink.title}
+                            </a>
+                          )
+                        )}
+                        <a
+                          href={`${experienc.companyInfo.companyWebSiteLink}`}
+                          class="text-info"
+                          target="_blank"
+                        >
                           {experienc.companyInfo.tileCompnayLink}
                         </a>
                       </div>
                     </div>
-                    <div className='card-body'>
-                      
-                        <div className='text-warning'>
-                        ** Skills Build **
+                    <div className="card-body">
+                      <div className="text-warning">** Skills Build **</div>
+                      <div className="text-success text-left">
+                        {experienc.skills}
                       </div>
-                      <div className='text-success text-left'>
-                           {experienc.skills}
-                      </div>
-                       
-                      
                     </div>
                   </div>
                 </div>
-                ))
-              }
-              </div>
-            </div>
+              ))}
           </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Experiencs
+export default Experiencs;
--- src/Components/ProjectTwo.js
+++ src/Components/ProjectTwo.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import airport from "../accets/images/homepage.jpg";
 import brand from "../accets/images/BrandPanle.PNG";
 
@@ -8,25 +8,28 @@ const projects = [
     title: "Binnys Jewellary Web-Site",
     skills: "React.js, Framer Motion, Three.js, Bootstrap",
     duration: "2024 Jan To Present",
-    link: "https://binnysgroups.netlify.app/",
+    link: "https://binnysjewellery.com/",
     description:
       "This website is developed for Binny's Jewelry, a company specializing in high-quality jewelry. The site showcases a wide range of jewelry items using modern web technologies.",
-    imageUrl:
-      "https://cfw42.rabbitloader.xyz/eyJjIjp0cnVlLCJoIjoiYnJpamRlc2lnbnN0dWRpby5jb20iLCJ2IjozMTMxNDg4NTM0LCJpIjoiNDllNjE3MTUtYjRhZC00OTA0LWZiYzktNjQyNzNlZDk2MzAwIn0/wp-content/uploads/2024/04/1-1.webp",
+    imageUrl: "https://binnysjewellery.com/images/Mumbai_Map.png",
   },
   {
     id: 2,
-    title: "Brand Panel for Restaurant",
-    skills: "React.js, Node.js, Express.js, Material UI, MSG-91, Nodemailer, Bootstrap",
-    duration: "2023 Aug To Present",
-    link: "http://businessportal.saveeat.in/",
-    description: "This dashboard is used by restaurants to manage their brand. It allows restaurants to add their products, manage choices, apply discounts, and more.",
-    imageUrl: brand
+    title: "Airline Reservation System",
+    skills:
+      "Node.js ,Express.js ,Moongodb , React JS, Bootstrap, Email.js and Axios",
+    duration: "Sep 2022 to Mar 2023",
+    link: "https://github.com/chetanbnagmoti/Air_System_Backend",
+    description:
+      "Book flights, manage reservations, and enjoy hassle-free journey arrangements, ensuring convenience and satisfaction for every passenger.",
+    imageUrl: airport, // Assuming airport is the variable holding the image URL
   },
+
   {
     id: 3,
     title: "Pizza Delivery System",
-    skills: "Node.js, React.js, Express.js, MongooDB, Socket.io, Nodemailer, RazorPay",
+    skills:
+      "Node.js, React.js, Express.js, MongooDB, Socket.io, Nodemailer, RazorPay",
     duration: "June 2023 to Present",
     link: "https://pizzaappchetannagmoti.netlify.app/",
     description:
@@ -43,7 +46,7 @@ const projects = [
     description:
       "Streamline alumni relations with comprehensive features for communication, events, donations, and networking.",
     imageUrl:
-      "https://itechindia.co/wp-content/uploads/2020/01/Blog-Image-2.png"
+      "https://itechindia.co/wp-content/uploads/2020/01/Blog-Image-2.png",
   },
   {
     id: 5,
@@ -70,7 +73,8 @@ const projects = [
   {
     id: 7,
     title: "Weather_App",
-    skills: "HTML, CSS, Bootstrap, and JavaScript. It leverages a third-party OpenWeatherMap API to provide real-time weather information to users.",
+    skills:
+      "HTML, CSS, Bootstrap, and JavaScript. It leverages a third-party OpenWeatherMap API to provide real-time weather information to users.",
     duration: "Sep 2022 to Mar 2023",
     link: "https://chetanbnagmoti.github.io/Weather_App/",
     description:
@@ -80,66 +84,71 @@ const projects = [
   },
   {
     id: 8,
-    title: "Airline Reservation System",
-    skills: "Spring Boot, My-SQL, HTML, CSS, React JS, Bootstrap, Email.js and Axios",
-    duration: "Sep 2022 to Mar 2023",
-    link: "https://github.com/chetanbnagmoti/CDAC-PROJECT-SEP-2022",
+    title: "Brand Panel for Restaurant",
+    skills:
+      "React.js, Node.js, Express.js, Material UI, MSG-91, Nodemailer, Bootstrap",
+    duration: "2023 Aug To Present",
+    // link: "http://businessportal.saveeat.in/",
     description:
-      "Book flights, manage reservations, and enjoy hassle-free journey arrangements, ensuring convenience and satisfaction for every passenger.",
-    imageUrl: airport, // Assuming airport is the variable holding the image URL
+      "This dashboard is used by restaurants to manage their brand. It allows restaurants to add their products, manage choices, apply discounts, and more.",
+    imageUrl: brand,
   },
   // Add more projects as needed
 ];
 
-
 const ProjectTwo = () => {
   return (
-   <>
-   <div className="col-lg-12 text-center resume mb-5">
-            <h1 className="pt-5 pb-2">Projects</h1>
-            <hr className="custom-hr" />
-            <p>
-              Below are the sample Data Analytics projects on Spring Boot, Java, API, Node.js, React.js, Express.js, Mongoo-Db, My-SQL, HTML, CSS.
-            </p>
-            <div className="container mt-5">
-              <div className="row justify-content-center">
-                {projects.map((project, index) => (
-                  <div
-                    key={index}
-                    className="col-lg-4 col-md-6 mb-3 d-flex justify-content-center"
-                  >
-                    <div
-                      className="text-white bg-dark shadow-lg"
-                      style={{ maxWidth: "100%" }}
-                    >
-                      <img
-                        src={project.imageUrl}
-                        className="card-img-top "
-                        alt="..."
-                        style={{ height: "200px" }}
-                      />
-                      <div className="card-body">
-                        <h5 className="card-title">{project.title}</h5>
-                       
-                        <p className="card-text text-muted">
-                          {project.description}
-                        </p>
-                        <a href={project.link} className="btn  btn-outline-warning" target="_blank">
-                          Visit
-                        </a>
-                        
-                         <p className="card-text text-muted text-left mt-2">Skill Used:- {project.skills}</p>
-                     
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-   </div>
-   
-   </>
-  )
-}
+    <>
+      <div className="col-lg-12 text-center resume mb-5">
+        <h1 className="pt-5 pb-2">Projects</h1>
+        <hr className="custom-hr" />
+        <p>
+          Below are the sample Data Analytics projects on Spring Boot, Java,
+          API, Node.js, React.js, Express.js, Mongoo-Db, My-SQL, HTML, CSS.
+        </p>
+        <div className="container mt-5">
+          <div className="row justify-content-center">
+            {projects.map((project, index) => (
+              <div
+                key={index}
+                className="col-lg-4 col-md-6 mb-3 d-flex justify-content-center"
+              >
+                <div
+                  className="text-white bg-dark shadow-lg"
+                  style={{ maxWidth: "100%" }}
+                >
+                  <img
+                    src={project.imageUrl}
+                    className="card-img-top "
+                    alt="..."
+                    style={{ height: "200px" }}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{project.title}</h5>
 
-export default ProjectTwo
+                    <p className="card-text text-muted">
+                      {project.description}
+                    </p>
+                    <a
+                      href={project.link}
+                      className="btn  btn-outline-warning"
+                      target="_blank"
+                    >
+                      Visit
+                    </a>
+
+                    <p className="card-text text-muted text-left mt-2">
+                      Skill Used:- {project.skills}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProjectTwo;
